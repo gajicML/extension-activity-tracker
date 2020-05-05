4d3e37708e307d77c51a8ff3518ec9c1c5e9601a
gajicML <mla.gajic@gmail.com>
2020-05-05 18:03:15 +0200

    update - processTabChange()

--- background.js
+++ background.js
@@ -21,5 +21,29 @@ function processTabChange() {
   // get active tabs
   chrome.tabs.query({ active: true }, (tabs) => {
     console.log("tabs", tabs);
+
+    // check if first active tab exist
+    if (tabs.length > 0 && tabs[0] !== null) {
+      let currentTab = tabs[0];
+      let url = currentTab.url;
+      let title = currentTab.title;
+      let hostName = url;
+
+      try {
+        let urlObject = new URL(url);
+        hostName = urlObject.hostname;
+      } catch (error) {
+        console.log(
+          `Could not construct url from ${currentTab.url}, error:${error}`
+        );
+      }
+
+      chrome.storage.local.get(
+        ["tabTimeObjectKey", "lastActiveTabKey"],
+        (result) => {
+          console.log("result", result);
+        }
+      );
+    }
   });
 }
